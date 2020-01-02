39cd98deebfa0aaccf0f7c17b9d648bb2699086f
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-01 22:02:27 -0300

    .NET Core 3.1 + .NET Standard + xUnit + Moq + Fluent Assertions
    .NET Core 3.1 + .NET Standard + xUnit + Moq + Fluent Assertions

---
+++ ConsultaCredito/AnaliseCredito.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsultaCredito
+{
+    public class AnaliseCredito
+    {
+        private IServicoConsultaCredito _servConsultaCredito;
+
+        public AnaliseCredito(IServicoConsultaCredito servConsultaCredito)
+        {
+            _servConsultaCredito = servConsultaCredito;
+        }
+
+        public StatusConsultaCredito ConsultarSituacaoCPF(string cpf)
+        {
+            try
+            {
+                var pendencias =
+                    _servConsultaCredito.ConsultarPendenciasPorCPF(cpf);
+
+                if (pendencias == null)
+                    return StatusConsultaCredito.ParametroEnvioInvalido;
+                else if (pendencias.Count == 0)
+                    return StatusConsultaCredito.SemPendencias;
+                else
+                    return StatusConsultaCredito.Inadimplente;
+            }
+            catch
+            {
+                return StatusConsultaCredito.ErroComunicacao;
+            }
+        }
+    }
+}
